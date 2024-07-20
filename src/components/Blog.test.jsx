322099f72f5a416e39ca76d654598046edce03b4
rjloube <rjloube@gmail.com>
2024-07-20 09:02:24 -0500

    5.14: Blog List Tests, step 2

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,29 +1,35 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 describe("<Blog />", () => {
-  // let container;
+  let container;
 
-  // beforeEach(() => {
-  //   const blog = {
-  //     title: "Component testing is done with react-testing-library",
-  //     author: "Test Author",
-  //     url: "http://www.example.com",
-  //   };
-
-  //   container = render(<Blog blog={blog} />).container;
-  // });
-
-  test("checks that blog renders title and author, but does not render URL or number of likes by default", async () => {
+  beforeEach(() => {
     const blog = {
       title: "Component testing is done with react-testing-library",
       author: "Test Author",
       url: "http://www.example.com",
       likes: 100,
+      user: {
+        username: "test-user-id",
+        name: "Test User Name",
+        id: "660177d1a3ecdcdb027ecf3b",
+      },
     };
 
-    const { container } = render(<Blog blog={blog} />);
+    const currentUser = {
+      username: "test-user-id",
+      name: "Test User Name",
+      id: "660177d1a3ecdcdb027ecf3b",
+    };
 
+    container = render(
+      <Blog blog={blog} currentUser={currentUser} />
+    ).container;
+  });
+
+  test("checks that blog renders title and author, but does not render URL or number of likes by default", async () => {
     const div = container.querySelector(".blog");
 
     expect(div).toHaveTextContent(
@@ -35,5 +41,15 @@ describe("<Blog />", () => {
     expect(div).not.toHaveTextContent("http://www.example.com");
 
     expect(div).not.toHaveTextContent("likes");
+  });
+
+  test("checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked", async () => {
+    const div = container.querySelector(".blog");
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+    screen.debug();
+    expect(div).toHaveTextContent("http://www.example.com");
+    expect(div).toHaveTextContent("likes");
   });
 });
